4b1c4375186d2f4dac98eb3370f85ed334fe1195
Rudi Hinds <rudihinds@Rudis-MacBook-Pro.local>
2023-05-02 20:54:06 +0100

    working next installation with amlify and tailwind configured

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.module = false;
+    }
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import "@aws-amplify/ui-react/styles.css";
+import "tailwindcss/tailwind.css";
+import dynamic from "next/dynamic";
+
+// const JitiModule = dynamic(() => import("jiti/dist/babel.js"), { ssr: false });
+// const TwinMacro = dynamic(() => import("twin.macro/macro.js"), { ssr: false });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
