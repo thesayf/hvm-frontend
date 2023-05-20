e401ad43465797f6cb770c5b7c2f17ab94cf5d73
Rudi Hinds <rudihinds@Rudis-MacBook-Pro.local>
2023-05-20 23:40:53 +0100

    add amplify configure to index to connect to api in amplify. graphql requests working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,18 @@ import type { AppProps } from "next/app";
 import "@aws-amplify/ui-react/styles.css";
 import "tailwindcss/tailwind.css";
 import CityCard from "@/components/CityCard";
+import { useEffect } from "react";
+import { Amplify } from "aws-amplify";
+import { listCityPrices } from "../../../hvm-api/src/graphql/queries";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const getCities = async () => {
+    const cities = await Amplify.API.graphql({
+      query: listCityPrices,
+    });
+    console.log(cities);
+  };
+  getCities();
   return (
     <>
       {/* <Component {...pageProps} /> */}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { Amplify } from "aws-amplify";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+Amplify.configure({
+  aws_appsync_graphqlEndpoint: "http://192.168.0.17:20002/graphql",
+  aws_appsync_region: "eu-west-1",
+  aws_appsync_authenticationType: "API_KEY",
+  aws_appsync_apiKey: "da2-fakeApiId123456",
+});
 
 export default function Home() {
-  return (
-    <h1>Home</h1>
-  )
+  return <h1>Home</h1>;
 }
